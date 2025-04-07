6d485a9244c533ee2ff868c772477eb4f6fd5b09
Dan <vareladanmarj@gmail.com>
2025-04-08 03:08:05 +0800

    feat: position pieces correctly on the board

--- src/components/board.ts
+++ src/components/board.ts
@@ -10,14 +10,24 @@ export class Board {
 
   protected boardRef: HTMLElement;
 
+  protected pieceStyle(square: Square) {
+    // prettier-ignore
+    const colIndex = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8 }[square[0]];
+    const rowIndex = parseInt(square[1]);
+
+    const leftInPercentage = (colIndex - 1) * (1 / 8) * 100;
+    const topInPercentage = (8 - rowIndex) * (1 / 8) * 100;
+
+    return {
+      top: `${topInPercentage}%`,
+      left: `${leftInPercentage}%`,
+    };
+  }
+
   get pieces() {
     return this.chessService
       .board()
       .flatMap((item) => item)
       .filter((item) => item);
-  }
-
-  get pieceStyle() {
-    return  {} 
   }
 }
