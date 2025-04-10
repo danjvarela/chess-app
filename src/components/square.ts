0740ae695ddd8f78b51380703e3e261ded425975
Dan <vareladanmarj@gmail.com>
2025-04-11 00:48:08 +0800

    refactor: use chess.js method for determining if square is light or dark

--- src/components/square.ts
+++ src/components/square.ts
@@ -1,26 +1,12 @@
-import { customElement, bindable } from "aurelia";
+import { IChessService } from "@/services/chess-service";
+import { customElement, bindable, resolve } from "aurelia";
 import { Square as ChessJsSquare } from "chess.js";
-
-type SquareType = "light" | "dark";
 
 @customElement("chess-square")
 export class Square {
   @bindable square: ChessJsSquare;
 
-  get type(): SquareType {
-    // prettier-ignore
-    const evenRowColors: Array<SquareType> = [ "light", "dark", "light", "dark", "light", "dark", "light", "dark" ];
-    // prettier-ignore
-    const oddRowColors: Array<SquareType> = [ "dark", "light", "dark", "light", "dark", "light", "dark", "light" ];
-    // prettier-ignore
-    const colIndex = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8 }[this.square[0]];
-
-    const rowIndex = parseInt(this.square[1]);
-
-    return rowIndex % 2 === 0
-      ? evenRowColors[colIndex - 1]
-      : oddRowColors[colIndex - 1];
-  }
+  private chessService = resolve(IChessService);
 
   protected get isAtTheLeftEdge() {
     return this.square[0] === "a";
@@ -31,13 +17,13 @@ export class Square {
   }
 
   protected get labelTextColor() {
-    return this.type === "light"
+    return this.chessService.squareColor(this.square) === "light"
       ? "var(--ca-color-teal-800)"
       : "var(--ca-color-teal-50)";
   }
 
   protected get background() {
-    return this.type === "light"
+    return this.chessService.squareColor(this.square) === "light"
       ? "var(--ca-color-teal-100)"
       : "var(--ca-color-teal-500)";
   }
